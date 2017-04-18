2f293080621dd92e17ba582ce2840ad6656f09b0
brandonsherbel <bsherbel@umich.edu>
2017-04-18 03:05:24 -0400

    more buttons

--- SwiftKeysWindowsCSharp/Form1.Designer.cs
+++ SwiftKeysWindowsCSharp/Form1.Designer.cs
@@ -35,6 +35,8 @@
             this.option1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.option2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.closeApplicationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restartBluetoothToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restartAndroidAppToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -53,36 +55,57 @@
             this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.option1ToolStripMenuItem,
             this.option2ToolStripMenuItem,
-            this.closeApplicationToolStripMenuItem});
+            this.closeApplicationToolStripMenuItem,
+            this.restartBluetoothToolStripMenuItem,
+            this.restartAndroidAppToolStripMenuItem});
             this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(202, 110);
+            this.contextMenuStrip1.Size = new System.Drawing.Size(333, 242);
             // 
             // option1ToolStripMenuItem
             // 
+            this.option1ToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 15F);
             this.option1ToolStripMenuItem.Name = "option1ToolStripMenuItem";
-            this.option1ToolStripMenuItem.Size = new System.Drawing.Size(201, 26);
+            this.option1ToolStripMenuItem.Size = new System.Drawing.Size(332, 42);
             this.option1ToolStripMenuItem.Text = "Show Predictions";
             this.option1ToolStripMenuItem.Click += new System.EventHandler(this.option1ToolStripMenuItem_Click);
             // 
             // option2ToolStripMenuItem
             // 
+            this.option2ToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 15F);
             this.option2ToolStripMenuItem.Name = "option2ToolStripMenuItem";
-            this.option2ToolStripMenuItem.Size = new System.Drawing.Size(201, 26);
+            this.option2ToolStripMenuItem.Size = new System.Drawing.Size(332, 42);
             this.option2ToolStripMenuItem.Text = "Show Controller";
             this.option2ToolStripMenuItem.Click += new System.EventHandler(this.option2ToolStripMeduItem_Click);
             // 
             // closeApplicationToolStripMenuItem
             // 
+            this.closeApplicationToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 15F);
             this.closeApplicationToolStripMenuItem.Name = "closeApplicationToolStripMenuItem";
-            this.closeApplicationToolStripMenuItem.Size = new System.Drawing.Size(201, 26);
+            this.closeApplicationToolStripMenuItem.Size = new System.Drawing.Size(332, 42);
             this.closeApplicationToolStripMenuItem.Text = "Close Application";
             this.closeApplicationToolStripMenuItem.Click += new System.EventHandler(this.closeApplication);
+            // 
+            // restartBluetoothToolStripMenuItem
+            // 
+            this.restartBluetoothToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 16F);
+            this.restartBluetoothToolStripMenuItem.Name = "restartBluetoothToolStripMenuItem";
+            this.restartBluetoothToolStripMenuItem.Size = new System.Drawing.Size(332, 42);
+            this.restartBluetoothToolStripMenuItem.Text = "Restart Bluetooth";
+            this.restartBluetoothToolStripMenuItem.Click += new System.EventHandler(this.restartBlueTooth);
+            // 
+            // restartAndroidAppToolStripMenuItem
+            // 
+            this.restartAndroidAppToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 16F);
+            this.restartAndroidAppToolStripMenuItem.Name = "restartAndroidAppToolStripMenuItem";
+            this.restartAndroidAppToolStripMenuItem.Size = new System.Drawing.Size(332, 42);
+            this.restartAndroidAppToolStripMenuItem.Text = "Restart Android App";
+            this.restartAndroidAppToolStripMenuItem.Click += new System.EventHandler(this.restartAndroidApp);
             // 
             // FootKeys
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(513, 253);
+            this.ClientSize = new System.Drawing.Size(750, 392);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FootKeys";
             this.Text = "Form1";
@@ -99,6 +122,8 @@
         private System.Windows.Forms.ToolStripMenuItem option1ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem option2ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem closeApplicationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restartBluetoothToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restartAndroidAppToolStripMenuItem;
     }
 }
 
